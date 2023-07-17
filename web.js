e58e83f83a832854e78ee3e4b7de00a1a367b80c
IvanTheMan2005 <139730534+IvanTheMan2005@users.noreply.github.com>
2023-07-17 13:13:54 +1000

    Add files via upload

---
+++ web.js
@@ -0,0 +1,15 @@
+var typed  = new Typed(".text", {
+    strings:['<i>Created by:</i> ', 'Ivan Castaneda.'],
+    typeSpeed:100,
+    backSpeed: 100,
+    backDelay:1000,
+    loop:true
+
+
+});
+
+
+/*var typed = new Typed('#element', {
+    strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+    typeSpeed: 50,
+  });*/
